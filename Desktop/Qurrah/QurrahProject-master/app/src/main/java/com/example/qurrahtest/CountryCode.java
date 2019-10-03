79e02d1d53079646cce8d16fb9fd87ac3b64a748
Sadeem Nasser <sadoomnasser@gmail.com>
2019-10-03 21:49:13 +0300

    add report type

--- Desktop/Qurrah/QurrahProject-master/app/src/main/java/com/example/qurrahtest/CountryCode.java
+++
@@ -1,8 +0,0 @@
-package com.example.qurrahtest;
-
-public class CountryCode {
-    public static final String[] countryNames = {"+996"};
-
-    public static final String[] countryAreaCodes = {"996"};
-
-}
