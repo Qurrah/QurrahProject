2ce6bb9e18732a00e4e4af531b32d4ecd53d678a
Reham <guzelreham@gmail.com>
2019-10-12 02:09:02 +0300

    Updating the GUI of the delete a report functionality

--- Desktop/QurrahProject-sprint-one/app/src/main/java/com/example/qurrah/MyReport.java
+++ Desktop/QurrahProject-sprint-one/app/src/main/java/com/example/qurrah/MyReport.java
@@ -2,14 +2,18 @@ package com.example.qurrah;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -18,6 +22,9 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.EventListener;
+
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
 
 public class MyReport extends AppCompatActivity {
 
@@ -25,9 +32,10 @@ public class MyReport extends AppCompatActivity {
     FirebaseAuth mAuth;
     String userID;
     RecyclerView recyclerView;
-    ArrayList<Report> list;
+    ArrayList<Report> list, newList;
     MyReportAdapter adapter;
     Button allbtn,missingbtn, findingbtn;
+    Report report;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +43,7 @@ public class MyReport extends AppCompatActivity {
         setContentView(R.layout.report_layout);
 //------------------------------------------------
         getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+        newList = new ArrayList<>();
 //------------------------------------------------
         // inputs
         mAuth = FirebaseAuth.getInstance();
@@ -80,11 +89,13 @@ public class MyReport extends AppCompatActivity {
                         findViewById(R.id.progressbar).setVisibility(View.GONE);
                     }
 
-                    Report report = ds.getValue(Report.class);
+                    report = ds.getValue(Report.class);
                     list.add(report);
 
                 }
+                newList.addAll(list);
                 adapter = new MyReportAdapter(MyReport.this, list);
+                new ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(recyclerView);
                 recyclerView.setAdapter(adapter);
                 findViewById(R.id.progressbar).setVisibility(View.GONE);
                 if(list.isEmpty()){
@@ -105,12 +116,11 @@ public class MyReport extends AppCompatActivity {
     }
     //-------------------------Second Filter Method---------------------------------
     public void SecondFilter(String flag){
-        ArrayList<Report> newList = new ArrayList<>();
 
+        newList.clear();
         switch(flag){
             case "all":
-                for(Report rep: list)
-                    newList.add(rep);
+                newList.addAll(list);
                 break;
             case"missing":
                 for(Report rep: list){
@@ -129,13 +139,87 @@ public class MyReport extends AppCompatActivity {
                 }
                 break;
         }
+
         adapter.updateList(newList);
+        recyclerView.scrollToPosition(adapter.getItemCount()-1);
 
     }
-//
+    //
+    Report deletedReport = null;
+    ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull final RecyclerView.ViewHolder viewHolder, int direction) {
+            final int position = viewHolder.getAdapterPosition();
+            deletedReport = newList.get(position);
+            System.out.println("Here"+position);
+            reference.addListenerForSingleValueEvent(new ValueEventListener() {
 
 
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+
+
+                    for (final DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        Report rep = snapshot.getValue(Report.class);
+                        if (deletedReport.getDate() == rep.getDate()) {
+                            reference.child(snapshot.getKey()).removeValue();
+                            newList.remove(position);
+                            adapter.notifyItemRangeChanged(position,newList.size());
+                            adapter.updateList(newList);
+
+                            final Snackbar snackBar = Snackbar.make(recyclerView, deletedReport.getLostTitle(), Snackbar.LENGTH_LONG);
+                            snackBar.setAction("تراجع", new View.OnClickListener() {
+
+                                @Override
+                                public void onClick(View v) {
+                                    snackBar.dismiss();
+                                    findViewById(R.id.noReports).setVisibility(View.GONE);
+                                    reference.child(snapshot.getKey()).setValue(deletedReport);
+                                    newList.add(position, deletedReport);
+                                    adapter.notifyItemInserted(position);
+                                    adapter.updateList(newList);
+
+
+
+
+                                }
+                            }).show();
+
+
+                        }
+
+
+
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+
+            });
+        }
+
+        @Override
+        public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+            new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                    .addSwipeRightBackgroundColor(ContextCompat.getColor(MyReport.this, R.color.darkRed))
+                    .addActionIcon(R.drawable.ic_delete_black_24dp)
+                    .addSwipeRightLabel("حذف")
+                    .create()
+                    .decorate();
+
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+    };
 }
+
 
 
 
