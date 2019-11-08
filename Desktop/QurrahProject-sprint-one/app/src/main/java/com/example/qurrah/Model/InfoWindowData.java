db6e0a1544cc3ee898b7a76b82c2f7552fe50d72
raghad <raghadm14@hotmail.com>
2019-11-08 19:24:19 +0300

    add temrs (need edit) + design a window Info marker

---
+++ Desktop/QurrahProject-sprint-one/app/src/main/java/com/example/qurrah/Model/InfoWindowData.java
@@ -0,0 +1,31 @@
+package com.example.qurrah.Model;
+
+public class InfoWindowData {
+    private String catogery;
+    private String type;
+    private String title;
+
+    public String getType() {
+        return type;
+    }
+
+    public String getCatogery() {
+        return catogery;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setCatogery(String catogery) {
+        this.catogery = catogery;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ Desktop/QurrahProject-sprint-one/app/src/main/java/com/example/qurrah/UI/terms.java
@@ -0,0 +1,19 @@
+package com.example.qurrah.UI;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.qurrah.R;
+
+public class terms extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.terms);
+
+    }
+
+}
